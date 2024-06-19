e2b20690929e9d40e532112d24b61257520dddd3
FreelancerTasin507 <freelancertasin507@gmail.com>
2024-06-19 14:31:53 +0600

    category section added

---
+++ src/pages/Category/Category.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { BsPersonWorkspace } from "react-icons/bs";
+import { FaPhoenixFramework } from "react-icons/fa";
+import { HiStatusOffline, HiStatusOnline } from "react-icons/hi";
+
+const Category = () => {
+  return (
+    <div>
+      <h1 className="text-center p-5 text-4xl font-bold mb-20">Category</h1>
+      <div className="flex justify-evenly gap-7">
+        <div className="card w-96 bg-slate-300 ">
+          <div className="card-body mx-auto">
+            <HiStatusOffline color="green" fontSize="4em" />
+            <h2 className="card-title">Offline</h2>
+          </div>
+        </div>
+        <div className="card w-96 bg-slate-300 ">
+          <div className="card-body mx-auto">
+            <HiStatusOnline color="green" fontSize="4em" />
+            <h2 className="card-title">Remote</h2>
+          </div>
+        </div>
+        <div className="card w-96 bg-slate-300 ">
+          <div className="card-body mx-auto">
+            <FaPhoenixFramework color="green" fontSize="4em" />
+            <h2 className="card-title">Part Time</h2>
+          </div>
+        </div>
+        <div className="card w-96 bg-slate-300 ">
+          <div className="card-body mx-auto">
+            <BsPersonWorkspace color="green" fontSize="4em" />
+            <h2 className="card-title">Full time</h2>
+          </div>
+        </div>
+      </div>
+    
+    </div>
+  );
+};
+
+export default Category;
