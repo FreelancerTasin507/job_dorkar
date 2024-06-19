3a902ab551b438c4d939b26188c2e611c8f03074
FreelancerTasin507 <freelancertasin507@gmail.com>
2024-06-19 14:33:54 +0600

    category section responsive

--- src/pages/Category/Category.jsx
+++ src/pages/Category/Category.jsx
@@ -7,26 +7,26 @@ const Category = () => {
   return (
     <div>
       <h1 className="text-center p-5 text-4xl font-bold mb-20">Category</h1>
-      <div className="flex justify-evenly gap-7">
-        <div className="card w-96 bg-slate-300 ">
+      <div className="grid lg:grid-cols-4 grid-cols-2  ml-5 md:ml-0 justify-evenly gap-7">
+        <div className="card md:w-96 w-40 bg-slate-300 ">
           <div className="card-body mx-auto">
             <HiStatusOffline color="green" fontSize="4em" />
             <h2 className="card-title">Offline</h2>
           </div>
         </div>
-        <div className="card w-96 bg-slate-300 ">
+        <div className="card md:w-96 w-40 bg-slate-300 ">
           <div className="card-body mx-auto">
             <HiStatusOnline color="green" fontSize="4em" />
             <h2 className="card-title">Remote</h2>
           </div>
         </div>
-        <div className="card w-96 bg-slate-300 ">
+        <div className="card md:w-96 w-40 bg-slate-300 ">
           <div className="card-body mx-auto">
             <FaPhoenixFramework color="green" fontSize="4em" />
             <h2 className="card-title">Part Time</h2>
           </div>
         </div>
-        <div className="card w-96 bg-slate-300 ">
+        <div className="card md:w-96 w-40 bg-slate-300 ">
           <div className="card-body mx-auto">
             <BsPersonWorkspace color="green" fontSize="4em" />
             <h2 className="card-title">Full time</h2>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Bannar from '../Bannar/Bannar';
 import Category from '../Category/Category';
+import Jobs from '../Jobs/Jobs';
 
 const Home = () => {
     return (
         <div>
             <Bannar></Bannar>
             <Category></Category>
+            <Jobs></Jobs>
         </div>
     );
 };
