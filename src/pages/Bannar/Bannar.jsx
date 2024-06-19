e2534fef772a9a4d452e71c43b3b7a7754a781c6
FreelancerTasin507 <freelancertasin507@gmail.com>
2024-06-19 13:10:05 +0600

    bannar tablet and mobile responsive complete

--- src/pages/Bannar/Bannar.jsx
+++ src/pages/Bannar/Bannar.jsx
@@ -8,13 +8,15 @@ const Bannar = () => {
       <section className="">
         <div className="bg-dark p-5">
           <div className="row flex justify-around items-center ">
-            <div className="">
-              <h4 className="text-4xl font-semibold">Helping you to find any type of job</h4>
+            <div className="md:w-[60%] lg:w-[50%]">
+              <h4 className="lg:text-4xl text-3xl font-semibold">
+                Helping you to find any type of job
+              </h4>
               <h1 className=" text-3xl font-bold mt-4">
                 Find Your Dream <br /> Job Today
               </h1>
               <div className="search-box">
-                <div className=" mt-5 flex space-x-4">
+                <div className=" mt-5 grid lg:grid-cols-4   md:grid-cols-3 gri md:space-x-4">
                   <div className=" flex items-center border border-gray-300 p-2 rounded">
                     <p>
                       <FaSearch className="text-gray-500 text-xl" />
@@ -22,20 +24,20 @@ const Bannar = () => {
                     <p className="input-text p-3 ml-4">search</p>
                   </div>
                   <div className=" small-box flex items-center border border-gray-300 p-2 rounded">
-                    <p className="input-text p-3">category</p>
-                    <p className="ml-4">
+                    <p className="">
                       <FaArrowAltCircleDown className="text-gray-500 text-xl" />
                     </p>
+                    <p className="input-text p-3">category</p>
                   </div>
                   <div className="location-input small-box flex items-center border border-gray-300 p-2 rounded">
-                    <p className="input-text p-3">location</p>
-                    <p className="ml-4">
+                    <p className="">
                       <FaMapMarkerAlt className="text-gray-500 text-xl" />
                     </p>
+                    <p className="input-text p-3">location</p>
                   </div>
                   <div className="text-white p-3">
                     <div className=" flex items-center bg-gray-800 text-white rounded p-2">
-                      <p className="mr-2 mt-2">
+                      <p className="mr-2 mt-2 ml-2">
                         <FaSearch className="text-gray-500 text-xl" />
                       </p>
                       <p className="p-2">search</p>
@@ -46,7 +48,7 @@ const Bannar = () => {
             </div>
             <div className="w-[40%]">
               <img
-                className="w-[80%]"
+                className="lg:w-[80%] hidden md:block"
                 src="https://i.ibb.co/F5MpXPv/9731139.jpg"
                 alt=""
               />
