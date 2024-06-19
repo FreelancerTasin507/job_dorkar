1405bf9c745a1a63a769eae5c42da6d43857cbdc
FreelancerTasin507 <freelancertasin507@gmail.com>
2024-06-19 12:28:47 +0600

    menubar responsive complete

--- src/pages/Menubar/Menubar.jsx
+++ src/pages/Menubar/Menubar.jsx
@@ -3,16 +3,49 @@ import { Link } from "react-router-dom";
 
 const Menubar = () => {
   return (
-    <div>
-      <div className="navbar bg-base-100 justify-between">
-        <a className="btn btn-ghost text-xl">Job Dorkar</a>
-        <div className="gap-5 font-semibold">
+    <div className="px-3 py-1">
+      <div className="navbar bg-base-100">
+        <div className="justify-start w-[70%]">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 gap-5 font-semibold pr-5 shadow bg-base-100 rounded-box w-52"
+            >
+              <Link>Home</Link>
+              <Link>My Job</Link>
+              <Link>Post Job</Link>
+              <Link>LogOut</Link>
+              <Link>Login</Link>
+              <Link>Registration</Link>
+            </ul>
+          </div>
+          <a className="btn btn-ghost text-xl">daisyUI</a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1 gap-10 font-semibold pr-5">
             <Link>Home</Link>
             <Link>My Job</Link>
             <Link>Post Job</Link>
             <Link>LogOut</Link>
             <Link>Login</Link>
             <Link>Registration</Link>
+          </ul>
         </div>
       </div>
     </div>
